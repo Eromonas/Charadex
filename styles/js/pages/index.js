9d635e2986f77a5bdd446e105a13befb48f1ec2f
Luna <90897209+LunasHere@users.noreply.github.com>
2026-02-08 14:11:58 -0600

    Remove prompts initialization from index.js
    Removed the prompts initialization and related logic from the DOMContentLoaded event listener.

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -9,28 +9,6 @@ import { charadex } from '../charadex.js';
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
 
-  /* Prompts
-  ===================================================================== */
-  let prompts = await charadex.initialize.page(null, charadex.page.index.prompts, (arr) => {
-
-      // Splice the silly little array
-      let sliceAmount = charadex.page.index.prompts.amount || 4;
-      arr.splice(sliceAmount, arr.length);
-
-    }, (data) => {
-
-      // Add the silly little prompt stuff here too
-      $('.cd-prompt-background').each(function(i) {
-        const element = $(this);
-        const image = data.array[i]?.image;
-        element.attr('style', `background-image: url(${image})`);
-      });
-      
-    }
-    
-  );
-
-
   /* Staff
   ===================================================================== */
   let staff = await charadex.initialize.page(null, charadex.page.index.staff, (arr) => {
